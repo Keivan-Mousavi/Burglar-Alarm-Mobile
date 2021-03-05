8fe8636e627042199b29760f10d6fd4fb56001f9
Keivan <keivan_mousavi@hotmail.com>
2021-03-05 23:32:05 +0330

    init project

---
+++ BurglarAlarmMobile/BurglarAlarmMobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using BurglarAlarmMobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace BurglarAlarmMobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ BurglarAlarmMobile/BurglarAlarmMobile/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using BurglarAlarmMobile.Models;
+using BurglarAlarmMobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BurglarAlarmMobile.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
