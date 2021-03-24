2b094e107f6ea3a93eaba8cffe41addeb5ba7d46
Keivan <keivan_mousavi@hotmail.com>
2021-03-25 02:49:41 +0430

    fix bug

--- BurglarAlarmMobile/BurglarAlarmMobile/ViewModels/LivingRoomViewModel.cs
+++ BurglarAlarmMobile/BurglarAlarmMobile/ViewModels/LivingRoomViewModel.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net.Http;
 using Xamarin.Forms;
 
 namespace BurglarAlarmMobile.ViewModels
@@ -17,7 +14,22 @@ namespace BurglarAlarmMobile.ViewModels
 
         private async void OnControllerTV(string sender)
         {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var responce = await client.GetAsync(string.Format("http://burglaralarm.persianprogrammer.com/ManageNotification/AddControllerTV?serial={0}&sendNEC={1}", "d6ac5b88-35e9-461f-b911-2f68d4cb9c44", sender));
 
+                    if (responce.IsSuccessStatusCode && responce.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        var json = await responce.Content.ReadAsStringAsync();
+                    }
+                }
+            }
+            catch
+            {
+
+            }
         } 
     }
 }
